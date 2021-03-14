ac3cf2ba0cf3d5905f51975616599acfd13802d2
codejet <social-os@mailbox.org>
2021-03-14 15:45:11 +0100

    Use different styling prefixes

--- container/src/App.js
+++ container/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <div>Hi there!</div>
-        <hr />
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <div>Hi there!</div>
+          <hr />
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
